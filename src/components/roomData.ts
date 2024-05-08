2587faf4cdaad565f3d6438ac75e309525ca30cb
JMDGO29 <92847204+JMDGO29@users.noreply.github.com>
2024-05-09 01:09:45 +0800

    Added room data for library

--- src/components/roomData.ts
+++ src/components/roomData.ts
@@ -3,12 +3,135 @@ import RegistrarToLibrary from "./Registrar to Library.jpg"
 import itachi from "./itachi.jpg"
 import naruto from "./naruto.jpg"
 
+// LIBRARY
+import libraryToIB101a from "../assets/Library/(LIBRARY) TO (IB101a).png"
+import libraryToIB102a from "../assets/Library/(LIBRARY) TO (IB102a).png"
+import libraryToIB103a from "../assets/Library/(LIBRARY) TO (IB103a).png"
+import libraryToIB104a from "../assets/Library/(LIBRARY) TO (IB104a).png"
+import libraryToIB105a from "../assets/Library/(LIBRARY) TO (IB105a).png"
+import libraryToIB106a from "../assets/Library/(LIBRARY) TO (IB106a).png"
+import libraryToIB107a from "../assets/Library/(LIBRARY) TO (IB107a).png"
+import libraryToIB108a from "../assets/Library/(LIBRARY) TO (IB108a).png"
+import libraryToIB109a from "../assets/Library/(LIBRARY) TO (IB109a).png"
+import libraryToIB110a from "../assets/Library/(LIBRARY) TO (IB110a).png"
+import libraryToIB201a from "../assets/Library/(LIBRARY) TO (IB201a).png"
+import libraryToIB202c from "../assets/Library/(LIBRARY) TO (IB202c).png"
+import libraryToIB203b from "../assets/Library/(LIBRARY) TO (IB203b).png"
+import libraryToIB204b from "../assets/Library/(LIBRARY) TO (IB204b).png"
+import libraryToIB205b from "../assets/Library/(LIBRARY) TO (IB205b).png"
+import libraryToIB206b from "../assets/Library/(LIBRARY) TO (IB206b).png"
+import libraryToIB207b from "../assets/Library/(LIBRARY) TO (IB207b).png"
+import libraryToIB208b from "../assets/Library/(LIBRARY) TO (IB208b).png"
+import libraryToIB209c from "../assets/Library/(LIBRARY) TO (IB209c).png"
+import libraryToIB210d from "../assets/Library/(LIBRARY) TO (IB210d).png"
+
 const roomData = {
     Library: {
         Registrar: {
             image: LibraryToRegistrar,
-            floorLevel: 2
-        },     
+            floorLevel: 2,
+            buildingName: "Admin"
+        },
+        IB101a: {
+            image: libraryToIB101a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB102a: {
+            image: libraryToIB102a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB103a: {
+            image: libraryToIB103a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB104a: {
+            image: libraryToIB104a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB105a: {
+            image: libraryToIB105a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB106a: {
+            image: libraryToIB106a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB107a: {
+            image: libraryToIB107a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB108a: {
+            image: libraryToIB108a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB109a: {
+            image: libraryToIB109a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB110a: {
+            image: libraryToIB110a,
+            floorLevel: 1,
+            buildingName: "Yellow"
+        },
+        IB201a: {
+            image: libraryToIB201a,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB202c: {
+            image: libraryToIB202c,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB203b: {
+            image: libraryToIB203b,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB204b: {
+            image: libraryToIB204b,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB205b: {
+            image: libraryToIB205b,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB206b: {
+            image: libraryToIB206b,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB207b: {
+            image: libraryToIB207b,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB208b: {
+            image: libraryToIB208b,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB209c: {
+            image: libraryToIB209c,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },
+        IB210d: {
+            image: libraryToIB210d,
+            floorLevel: 2,
+            buildingName: "Yellow"
+        },           
     },
     Registrar: {
         Library: {
