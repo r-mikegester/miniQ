00aa01beaa16081bc6cffc0b9d4bbcfde2f393a1
JMDGO29 <92847204+JMDGO29@users.noreply.github.com>
2024-05-09 11:48:44 +0800

    Fixed error on search query

--- src/components/WelcomeComponent.tsx
+++ src/components/WelcomeComponent.tsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
 import { IonContent, IonPage, IonModal, IonButton, IonSearchbar, IonBackButton, IonButtons, IonTitle, IonToolbar, IonIcon, IonNavLink, IonHeader, IonFooter } from "@ionic/react";
-import { chevronBackOutline, qrCodeOutline, ellipsisHorizontal, ellipsisVertical, personCircle, search } from "ionicons/icons";
+import { chevronBackOutline, qrCodeOutline } from "ionicons/icons";
 import { useHistory, useParams } from "react-router";
-import roomData from "./roomData"; // Assuming roomData is exported from another file
-import Home from "../pages/Home";
+import roomData from "./roomData";
 
 interface RoomDetails {
   image: string;
   floorLevel: string;
   buildingName: string;
-  // Add more properties as needed
 }
 
 const WelcomeComponent = () => {
     const modal = useRef<HTMLIonModalElement>(null);
+    const [isOpen, setIsOpen] = useState(false);
 
     const history = useHistory();
     const { qrValue } = useParams<{ qrValue: keyof typeof roomData }>();
@@ -28,31 +27,22 @@ const WelcomeComponent = () => {
         null
     );
 
-  useEffect(() => {
-    // Filter destinationData based on searchQuery
-    if (destinationData) {
-      const filtered = Object.keys(destinationData)
-        .filter((key) => {
-          const roomDetails = destinationData[key];
-          // Check if destination or building name includes the search query
-          return (
-            key.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            roomDetails.buildingName.toLowerCase().includes(searchQuery.toLowerCase())
-          );
-        })
-        .reduce<{ [key: string]: RoomDetails }>((obj, key) => {
-          obj[key] = destinationData[key];
-          return obj;
-        }, {});
-      setFilteredData(filtered);
-    }
-  }, [searchQuery, destinationData]);  
+    useEffect(() => {
+        const filteredData = roomData[qrValue];
+        setDestinationData(filteredData);
+        setFilteredData(filteredData);
+    }, [qrValue]);
 
     useEffect(() => {
-        // Filter destinationData based on searchQuery
         if (destinationData) {
             const filtered = Object.keys(destinationData)
-                .filter((key) => key.toLowerCase().includes(searchQuery.toLowerCase()))
+                .filter((key) => {
+                    const roomDetails = destinationData[key];
+                    return (
+                        key.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                        roomDetails.buildingName.toLowerCase().includes(searchQuery.toLowerCase())
+                    );
+                })
                 .reduce<{ [key: string]: RoomDetails }>((obj, key) => {
                     obj[key] = destinationData[key];
                     return obj;
@@ -73,39 +63,51 @@ const WelcomeComponent = () => {
         setSelectedDestination(null);
     };
 
+    const handleModalDidDismiss = () => {
+        setIsOpen(false); // Ensure isOpen is set to false when the modal is dismissed
+    };
+
+    const handleBack = () => {
+        history.push('/home');
+      };
+
+      
     return (
         <IonPage>
             <IonFooter>
                 <IonToolbar>
                     <IonButtons slot="start">
-                        <IonBackButton defaultHref="/home"></IonBackButton>
+                        <IonButton onClick={handleBack}><IonIcon slot="icon-only" icon={chevronBackOutline} />Back</IonButton>
                     </IonButtons>
                     <IonTitle></IonTitle>
                 </IonToolbar>
             </IonFooter>
             <IonContent>
-
                 <div id="container" className="z-40 flex items-center justify-center w-full h-full">
                     <div className="flex flex-col justify-between w-full h-full p-6 bg-base-300 text-base-content">
                         <div className="flex flex-col items-center justify-center">
                             <h1 className='text-xl'>Your Current Location:</h1>
                             <h1 className="-mt-2 text-6xl font-bold text-center w-96 rounded-2xl">{qrValue}</h1>
                         </div>
-
-                        <IonModal ref={modal} trigger="open-modal" mode='ios' initialBreakpoint={0.75} breakpoints={[0, 0.75]}>
+                        <IonModal
+                            ref={modal}
+                            mode='ios'
+                            initialBreakpoint={0.75}
+                            breakpoints={[0, 0.75]}
+                            isOpen={isOpen}
+                            onDidDismiss={handleModalDidDismiss} // Listen for modal dismissal event
+                        >
                             <IonFooter>
                                 <IonToolbar>
                                     <IonButtons slot="secondary">
                                         <IonButton onClick={handleCloseModal}>
                                             <IonIcon slot="icon-only" className="text-white" icon={chevronBackOutline}></IonIcon>
                                         </IonButton>
-
                                     </IonButtons>
                                     <IonTitle></IonTitle>
                                 </IonToolbar>
                             </IonFooter>
                             <IonContent className=" ion-padding">
-                                {/* <IonSearchbar onClick={() => modal.current?.setCurrentBreakpoint(0.75)} showCancelButton="focus" animated={true} placeholder="&nbsp; &nbsp; &nbsp; Where do you want to go?" className="w-full h-16 text-left"></IonSearchbar> */}
                                 <div className="px-3">
                                     <input
                                         className="w-full px-10 py-5 mt-5 text-base-content h-14 bg-base-100 rounded-xl"
@@ -114,28 +116,23 @@ const WelcomeComponent = () => {
                                         placeholder="Search destination"
                                     />
                                 </div>
-
+                                <h1 className="mb-5 text-center">Search Results:</h1>
                                 <div className="flex flex-col p-3 mt-10 h-[600px]">
                                     <div>
-
                                         {searchQuery &&
                                             filteredData &&
                                             Object.keys(filteredData).length === 0 && <h1 className="text-center">No results found.</h1>}
-
                                         {searchQuery &&
                                             filteredData &&
                                             Object.keys(filteredData).length > 0 && (
-
                                                 <div>
                                                     {Object.entries(filteredData).map(([destination, details]) => (
                                                         <div key={destination}>
-                                                            <h1 className="mb-5 text-center">Results:</h1>
                                                             <button
                                                                 onClick={() => handleDestinationClick(destination)}
-                                                                className="flex items-center justify-center btn-block btn"
+                                                                className="flex items-center justify-center btn-block btn mb-3 h-auto"
                                                             >
-
-                                                                <h3 className="font-semibold uppercase">{destination}</h3>
+                                                                <h3 className="font-semibold uppercase">{destination} - floor: {details.floorLevel} - {details.buildingName}</h3>
                                                             </button>
                                                             <IonModal isOpen={selectedDestination === destination} mode="ios">
                                                                 <IonFooter>
@@ -144,18 +141,18 @@ const WelcomeComponent = () => {
                                                                             <IonButton onClick={handleCloseModal}>
                                                                                 <IonIcon slot="icon-only" className="text-white" icon={chevronBackOutline}></IonIcon>
                                                                             </IonButton>
-
                                                                         </IonButtons>
                                                                         <IonButtons slot="primary">
-
-                                                                            <IonButton onClick={() => {
-                                                                                handleCloseModal();
-                                                                                history.push("/home");
-
-                                                                            }}>
-                                                                                <IonIcon slot="icon-only" className="text-white" ios={qrCodeOutline} md={qrCodeOutline}></IonIcon>
+                                                                            <IonButton
+                                                                                onClick={() => {
+                                                                                    handleCloseModal();
+                                                                                    history.push("/home");
+                                                                                    setIsOpen(false);
+                                                                                    setSearchQuery("");
+                                                                                }}
+                                                                            >
+                                                                                <IonIcon slot="icon-only" className="text-white" icon={qrCodeOutline}></IonIcon>
                                                                             </IonButton>
-
                                                                         </IonButtons>
                                                                         <IonTitle></IonTitle>
                                                                     </IonToolbar>
@@ -164,8 +161,6 @@ const WelcomeComponent = () => {
                                                                     <img src={details.image} alt={destination} className="h-full" />
                                                                 </IonContent>
                                                             </IonModal>
-                                                            {/* <h2>Floor Level: {details.floorLevel}</h2> */}
-                                                            {/* Assuming there are other details to display */}
                                                         </div>
                                                     ))}
                                                 </div>
@@ -175,27 +170,20 @@ const WelcomeComponent = () => {
                             </IonContent>
                         </IonModal>
                         <div>
-                            <h1 className="text-center">where do you want to go?</h1>
+                            <h1 className="text-center">Where do you want to go?</h1>
                             <div>
-                                {/* <input type='text' className='w-full h-16 p-6 rounded-2xl bg-base-300' /> */}
-                                <IonButton id="open-modal" expand="block" className="z-50 w-full rounded-3xl searchBtn">
+                                <IonButton
+                                    id="open-modal"
+                                    expand="block"
+                                    className="z-50 w-full rounded-3xl searchBtn"
+                                    onClick={() => setIsOpen(true)}
+                                >
                                     Search Your Destination
                                 </IonButton>
                             </div>
                         </div>
                     </div>
                 </div>
-
-
-
-                {/* <button
-                    onClick={() => {
-                        history.push("/home");
-                    }}
-                    className="btn"
-                >
-                    BACK TO HOME to SCAN AGAIN
-                </button> */}
             </IonContent>
         </IonPage>
     );
