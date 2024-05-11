70db72745a2b41c454cba3742da292c5ada89682
powerrangerrrr <91194039+powerrangerrrr@users.noreply.github.com>
2024-05-12 03:16:57 +0800

    Physics and Chemistry Lab Room to Room Data

---
+++ src/data/PhysicsLab.ts
@@ -0,0 +1,68 @@
+
+// ADMIN BUILDING
+import libraryToLobby from "../assets/PhysicsLab/(LIBRARY) TO (A-Admin Lobby).png"
+import libraryToAccounting from "../assets/PhysicsLab/(LIBRARY) TO (A-Accounting Division).png"
+import libraryToAdmission from "../assets/PhysicsLab/(LIBRARY) TO (A-Admission Office).png"
+import libraryToRegistrar from "../assets/PhysicsLab/(LIBRARY) TO (A-Registrar Office).png"
+import libraryToHR from "../assets/PhysicsLab/(LIBRARY) TO (A-HR Office).png"
+
+// CHED BUILDING
+import libraryToCashier from "../assets/PhysicsLab/(LIBRARY) TO (C-Cashier).png"
+import libraryToExecutive from "../assets/PhysicsLab/(LIBRARY) TO (C-Executive Office).png"
+import libraryToScholarship from "../assets/PhysicsLab/(LIBRARY) TO (C-Scholarship Unit).png"
+
+// TECHVOC
+import libraryToGym from "../assets/PhysicsLab/(LIBRARY) TO (IA-QCU Gym).png"
+import libraryToInnopad from "../assets/PhysicsLab/(LIBRARY) TO (IA-QCU Innopad).png"
+import libraryToMotorpool from "../assets/PhysicsLab/(LIBRARY) TO (IA-Motorpool Unit Office).png"
+import libraryToIA203 from "../assets/PhysicsLab/(LIBRARY) TO (IA203-PFGSD).png"
+import libraryToIA206 from "../assets/PhysicsLab/(LIBRARY) TO (IA206-SPARDS).png"
+import libraryToIA207 from "../assets/PhysicsLab/(LIBRARY) TO (IA207-SASD).png"
+import libraryToIA208 from "../assets/PhysicsLab/(LIBRARY) TO (IA208-ICTO).png"
+import libraryToIA209 from "../assets/PhysicsLab/(LIBRARY) TO (IA209-ICTO).png"
+import libraryToIA210 from "../assets/PhysicsLab/(LIBRARY) TO (IA210-QACIMDO).png"
+
+// ACADEMIC BUILDING
+import AccountancyToClinic from "../assets/PhysicsLab/(AccountancyDept) TO (Clinic).png"
+import AccountancyToLibrary from "../assets/PhysicsLab/(AccountancyDept) TO (Library).png"
+import AccountancyToCPCE from "../assets/PhysicsLab/(AccountancyDept) TO (CPCE).png"
+// ACADEMIC BUILDING
+
+
+const PhysicsLab = {
+    // ADMIN BUILDING
+    Admin_Lobby: {image: libraryToLobby, floorLevel: "1", buildingName: "Admin", roomName: "Admin Lobby"},
+    Accounting_Division: {image: libraryToAccounting, floorLevel: "2", buildingName: "Admin", roomName: "Accounting Division"},
+    Admission_Office: {image: libraryToAdmission, floorLevel: "2", buildingName: "Admin", roomName: "Admission Office"},
+    Registrar_Office: {image: libraryToRegistrar, floorLevel: "2", buildingName: "Admin", roomName: "Registrar Office"},
+    HR_Office: {image: libraryToHR, floorLevel: "2", buildingName: "Admin", roomName: "HR Office"},
+    // ADMIN BUILDING
+
+    // CHED
+    Cashier: {image: libraryToCashier, floorLevel: "1", buildingName: "CHED", roomName: "Cashier"},
+    Executive_Office: {image: libraryToExecutive, floorLevel: "1", buildingName: "CHED", roomName: "Executive Office"},
+    Scholarship_Unit: {image: libraryToScholarship, floorLevel: "1", buildingName: "CHED", roomName: "Scholarship Unit"},
+    // CHED
+    
+
+    // TECHVOC BUILDING
+    QCU_Gym: {image: libraryToGym, floorLevel: "1", buildingName: "Techvoc", roomName: "QCU Gym"},
+    QCU_Innopad: {image: libraryToInnopad, floorLevel: "1", buildingName: "Techvoc", roomName: "QCU Innopad"},
+    Motorpool_Unit_Office: {image: libraryToMotorpool, floorLevel: "1", buildingName: "Techvoc", roomName: "Motorpool Unit Office"},
+
+    IA203_PFGSD: {image: libraryToIA203, floorLevel: "2", buildingName: "Techvoc", roomName: "PFGSD - IA203"},
+    IA206_SPARDS: {image: libraryToIA206, floorLevel: "2", buildingName: "Techvoc", roomName: "SPARDS - IA206"},
+    IA207_SASD: {image: libraryToIA207, floorLevel: "2", buildingName: "Techvoc", roomName: "SASD - IA207"},
+    IA208_ICTO: {image: libraryToIA208, floorLevel: "2", buildingName: "Techvoc", roomName: "ICTO - IA208"},
+    IA209_ICTO: {image: libraryToIA209, floorLevel: "2", buildingName: "Techvoc", roomName: "ICTO - IA209"},
+    IA210_QACIMDO: {image: libraryToIA210, floorLevel: "2", buildingName: "Techvoc", roomName: "QACIMDO - IA210"},
+    // TECHVOC BUILDING
+
+    // ACADEMIC BUILDING
+    Clinic: {image: AccountancyToClinic, floorLevel: "1", buildingName: "Admin", roomName: "Medical and Dental Clinic"},
+    CPCE: {image: AccountancyToCPCE, floorLevel: "1", buildingName: "Admin", roomName: "Center for Preofessional and Continuing Education"},
+    Library: {image: AccountancyToLibrary, floorLevel: "1", buildingName: "Admin", roomName: "Library"},
+    // ACADEMIC BUILDING 
+};
+
+export default PhysicsLab;
