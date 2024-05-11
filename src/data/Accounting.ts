0d5bae60c8d4fbe024fc023b5855a1b2c281751e
julscomighod <146498967+julscomighod@users.noreply.github.com>
2024-05-11 23:33:05 +0800

    Admin to Faculties (Only Faculties)

---
+++ src/data/Accounting.ts
@@ -0,0 +1,100 @@
+import AdmissionToAccountingChed from "../assets/Registrar/(REGISTRAR) TO (REGISTRAR TO ACCOUNTANCY).png"
+import AdmissionToCashier from "../assets/Registrar/REGISTRAR TO COOP (2).png"
+import AdmissionToExecutive from "../assets/Registrar/(REGISTRAR) TO (C-Executive Office).png"
+import AdmissionToRecords from "../assets/Registrar/(REGISTRAR) TO (C-Records Unit).png"
+import AdmissionToScholarship from "../assets/Registrar/(REGISTRAR) TO (C-Scholarship Unit).png"
+import AccountingToCanteen from "../assets/Accounting/ACCOUNTING TO CANTEEN.png"
+import AccountingToCoop from "../assets/Accounting/ACCOUNTING TO COOP.png"
+import AccountingToAccountancyDept from "../assets/Accounting/ACCOUNTING TO ACCOUNTANCY.png"
+import AccountingToChemLab from "../assets/Accounting/ACCOUNTING TO CHEM LAB.png"
+import AccountingToPhysicsLab from "../assets/Accounting/ACCOUNTING TO PHYSICS.png"
+import AccountingToEngrOffice from "../assets/Accounting/ACCOUNTING TO ENGINEERING.png"
+import AccountingToElectronicLab from "../assets/Accounting/ACCOUNTING TO ELECTRONIC.png"
+import AccountingToIK203 from "../assets/Accounting/ACCOUNTING TO IK203.png"
+import AccountingToIK204 from "../assets/Accounting/ACCOUNTING TO IK204.png"
+import AccountingToEducDept from "../assets/Accounting/ACCOUNTING TO EDUC.png"
+import AccountingToMathAndScience from "../assets/Accounting/ACCOUNTING TO MATHSCIE.png"
+import AccountingToIK303 from "../assets/Accounting/ACCOUNTING TO IK303.png"
+import AccountingToIK304 from "../assets/Accounting/BLANK.png"
+import AccountingToEntrepDept from "../assets/Accounting/ACCOUNTING TO ENTREP.png"
+import AccountingToIK403 from "../assets/Accounting/ACCOUNTING TO IK403.png"
+import AccountingToIK404 from "../assets/Accounting/ACCOUNTING TO IK404.png"
+import AccountingToEngrDept from "../assets/Accounting/ACCOUNTING TO COE.png"
+import AccountingToEngrLounge from "../assets/Accounting/ACCOUNTING TO LOUNGE.png"
+import AccountingToIK503 from "../assets/Accounting/ACCOUNTING TO IK503.png"
+import AccountingToIK504 from "../assets/Accounting/ACCOUNTING TO IK504.png"
+import AccountingToCCS from "../assets/Accounting/ACCOUNTING TO CCS.png"
+import AccountingToITSO from "../assets/Accounting/ACCOUNTING TO ITSO.png"
+import AccountingToIK603 from "../assets/Accounting/ACCOUNTING TO IK603.png"
+import AccountingToIK604 from "../assets/Accounting/ACCOUNTING TO IK604.png"
+
+import AccountingToGym from "../assets/Accounting/ACCOUNTING TO GYM.png"
+import AccountingToIA104c from "../assets/Accounting/ACCOUNTING TO IA104C.png"
+import AccountingToIA105 from "../assets/Accounting/ACCOUNTING TO IA105.png"
+import AccountingToIA106b from "../assets/Accounting/ACCOUNTING TO IA106b.png"
+import AccountingToIA107b from "../assets/Accounting/ACCOUNTING TO IA107b.png"
+import AccountingToInnopad from "../assets/Accounting/ACCOUNTING TO INNOPAD.png"
+import AccountingToMotorpool from "../assets/Accounting/ACCOUNTING TO MOTORPOOL.png"
+import AccountingToPFGSD from "../assets/Accounting/ACCOUNTING TO ICTO PFGSD IA203.png"
+import AccountingToQACIMDO from "../assets/Accounting/ACCOUNTING TO ICTO QACIMDO IA210.png"
+import AccountingToSASD from "../assets/Accounting/ACCOUNTING TO ICTO SASD IA207.png"
+import AccountingToSPARD from "../assets/Accounting/ACCOUNTING TO ICTO SPARD IA206.png"
+import AccountingToICTO208 from "../assets/Accounting/ACCOUNTING TO ICTO208.png"
+import AccountingToIA202 from "../assets/Accounting/ACCOUNTING TO IA202.png"
+import AccountingToIA204 from "../assets/Accounting/ACCOUNTING TO IA204.png"
+
+import AccountingToGuidance from "../assets/Accounting/ACCOUNTINGTO GUIDANCE.png"
+import AccountingToClinic from "../assets/Accounting/ACCOUNTINGTO CLINIC.png"
+import AccountingToLibrary from "../assets/Accounting/ACCOUNTINGTO LIBRARY.png"
+import { library } from "ionicons/icons"
+
+const Accounting = {
+    Accounting: {
+        // Bautista Room
+        Canteen: {image: AccountingToCanteen, floorLevel: "Basement", buildingName: "Bautista"},
+        Coop: {image: AccountingToCoop, floorLevel: "Basement", buildingName: "Bautista"},
+        Accountancy_Department: {image: AccountingToAccountancyDept, floorLevel: "Ground Floor", buildingName: "Bautista"},
+        Chemistry_Laboratory: {image:AccountingToChemLab, floorLevel: "Ground Floor", buildingName: "Bautista"},
+        Physics_Laboratory: {image: AccountingToPhysicsLab, floorLevel: "Ground Floor", buildingName: "Bautista"},
+        Engineering_Lab_Office: {image: AccountingToEngrOffice, floorLevel: "2", buildingName: "Bautista"},
+        Electronic_Lab: {image: AccountingToElectronicLab, floorLevel: "2", buildingName: "Bautista"},
+        IK203: {image: AccountingToIK203, floorLevel: "2", buildingName: "Bautista"},
+        IK204: {image: AccountingToIK204, floorLevel: "2", buildingName: "Bautista"},
+        College_of_Education: {image: AccountingToEducDept, floorLevel: "3", buildingName: "Bautista"},
+        Math_and_Science_Department: {image: AccountingToMathAndScience, floorLevel: "3", buildingName: "Bautista"},
+        IK303: {image: AccountingToIK303, floorLevel: "3", buildingName: "Bautista"},
+        IK304: {image: AccountingToIK304, floorLevel: "3", buildingName: "Bautista"},
+        Entrepreneurship_Department: {image: AccountingToEntrepDept, floorLevel: "4", buildingName: "Bautista"},
+        IK403: {image: AccountingToIK403, floorLevel: "4", buildingName: "Bautista"},
+        IK404: {image: AccountingToIK404, floorLevel: "4", buildingName: "Bautista"},
+        College_of_Enginering: {image: AccountingToEngrDept, floorLevel: "5", buildingName: "Bautista"},
+        Engineering_Lounge: {image: AccountingToEngrLounge, floorLevel: "5", buildingName: "Bautista"},
+        IK503: {image: AccountingToIK503, floorLevel: "5", buildingName: "Bautista"},
+        IK504: {image: AccountingToIK504, floorLevel: "5", buildingName: "Bautista"},
+        College_of_Computer_Studies: {image: AccountingToCCS, floorLevel: "6", buildingName: "Bautista"},
+        Innovation_and_Technology_Support_Office: {image: AccountingToITSO, floorLevel: "6", buildingName: "Bautista"},
+        IK603: {image: AccountingToIK603, floorLevel: "6", buildingName: "Bautista"},
+        IK604: {image: AccountingToIK604, floorLevel: "6", buildingName: "Bautista"},
+
+        // TECH-VOC ROOM
+        QCU_Gym: {image: AccountingToGym, floorLevel: "1", buildingName: "Techvoc"},
+        IA104c: {image: AccountingToIA104c, floorLevel: "1", buildingName: "Techvoc"},
+        IA105: {image: AccountingToIA105, floorLevel: "1", buildingName: "Techvoc"},
+        IA106b: {image: AccountingToIA106b, floorLevel: "1", buildingName: "Techvoc"},
+        IA107b: {image: AccountingToIA107b, floorLevel: "1", buildingName: "Techvoc"},
+        QCU_Innopad: {image: AccountingToInnopad, floorLevel: "1", buildingName: "Techvoc"},
+        Motorpool_Unit_Office: {image: AccountingToMotorpool, floorLevel: "1", buildingName: "Techvoc"},
+        PFGSD_IA203: {image: AccountingToPFGSD, floorLevel: "2", buildingName: "Techvoc"},
+        QACIMDO_210: {image: AccountingToQACIMDO, floorLevel: "2", buildingName: "Techvoc"},
+        SASD_IA207: {image: AccountingToSASD, floorLevel: "2", buildingName: "Techvoc"},
+        SPARD_IA206: {image: AccountingToSPARD, floorLevel: "2", buildingName: "Techvoc"},
+        ICTO208: {image: AccountingToICTO208, floorLevel: "2", buildingName: "Techvoc"},
+        IA202: {image: AccountingToIA202, floorLevel: "2", buildingName: "Techvoc"},
+        IA204: {image: AccountingToIA204, floorLevel: "2", buildingName: "Techvoc"},
+
+        // Academic Room
+        Guidance: {image: AccountingToGuidance, floorLevel: "1", buildingName: "Academic"},
+        Clinic: {image: AccountingToClinic, floorLevel: "1", buildingName: "Academic"},
+        Library: {image: AccountingToLibrary, floorLevel: "1", buildingName: "Academic"},
+    }
+}
